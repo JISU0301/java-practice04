9fde9ed36402fd3f5b31954ca021af1fc868d36a
BIT <BIT@DESKTOP-NMF9F4U>
2019-08-12 14:13:25 +0900

    완료

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -2,5 +2,22 @@ package prob01;
 
 public class Printer{
 
+	public void println(int b) {
+		System.out.println(b);
+		
+	}
+	public void println(boolean b) {
+		System.out.println(b);
+		
+	}
 	
+	public void println(double b) {
+		System.out.println(b);
+		
+	}
+	
+	public void println(String b) {
+		System.out.println(b);
+		
+	}
 }
---
+++ src/prob03/Unit.java
@@ -0,0 +1,9 @@
+package prob03;
+
+public class Unit {
+	void move(int x, int y) {
+}
+	void stop( ) {
+		
+	}
+}
